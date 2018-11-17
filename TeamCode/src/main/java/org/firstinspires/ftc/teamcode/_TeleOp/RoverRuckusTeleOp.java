23539618b9ab56ad5d38bad9a0003c3724529916
Scott3.0 <bremmerscottw@gmail.com>
2018-11-16 19:29:29 -0800

    Lkift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/RoverRuckusTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/RoverRuckusTeleOp.java
@@ -20,6 +20,8 @@ public class RoverRuckusTeleOp extends OpMode{
     double left, right;
     double speedFactor = 0.5;
     double liftSpeed = 1;
+    int brakeCount = 0;
+    boolean liftBrake = false;
     DecimalFormat printFormat = new DecimalFormat ("#.###");
     @Override
     public void init() {
@@ -28,22 +30,27 @@ public class RoverRuckusTeleOp extends OpMode{
 
     @Override
     public void loop() {
-        if(gamepad1.left_bumper && !gamepad1.right_bumper){ //Drivetrain Speed Controls
-            speedFactor = 1; }
-        else if(gamepad1.right_bumper && !gamepad1.left_bumper){
-            speedFactor = 0.25; }
-        else if(!gamepad1.right_bumper && !gamepad1.left_bumper){
-            speedFactor = 0.5; }
+        if(gamepad1.left_bumper && !gamepad1.right_bumper) //Drivetrain Speed Controls
+            speedFactor = 1;
+        else if(gamepad1.right_bumper && !gamepad1.left_bumper)
+            speedFactor = 0.25;
+        else if(!gamepad1.right_bumper && !gamepad1.left_bumper)
+            speedFactor = 0.5;
 
-        if(gamepad2.left_bumper && !gamepad2.right_bumper){ //Lift Controls
+        if(gamepad2.left_bumper && !gamepad2.right_bumper) {//Lift Controls
             robot.lift.setPower(liftSpeed);
-            robot.lift2.setPower(liftSpeed); }
+            robot.lift2.setPower(liftSpeed);}
         else if(gamepad2.right_bumper && !gamepad2.left_bumper){
-            robot.lift.setPower(-1 * liftSpeed);
-            robot.lift2.setPower(-1 * liftSpeed); }
+        robot.lift.setPower(-1);
+        robot.lift2.setPower(-1 * liftSpeed); }
         else if(!gamepad2.right_bumper && !gamepad2.left_bumper){
             robot.lift.setPower(0);
             robot.lift2.setPower(0); }
+
+        if(gamepad2.b) {//Lift Controls
+            robot.lift.setPower(0);
+            robot.lift2.setPower(0);}
+
 
 
         left = (-1)* Math.pow(gamepad1.left_stick_y, 3) * speedFactor;
@@ -61,6 +68,7 @@ public class RoverRuckusTeleOp extends OpMode{
         telemetry.addData("Right: ", printFormat.format(right));
         telemetry.addData("Lift Motor 1: ", printFormat.format(robot.lift.getPower()));
         telemetry.addData("Lift Motor 2: ", printFormat.format(robot.lift2.getPower()));
+        telemetry.addData("Lift Braked: ", gamepad2.b);
 
     }
 }

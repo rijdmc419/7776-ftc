5a68b885a966244230cf60e3f4b47332e8b7c853
Scott3.0 <bremmerscottw@gmail.com>
2018-12-22 13:09:54 -0800

    Rebuilt, like the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/RoverRuckusTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/RoverRuckusTeleOp.java
@@ -19,37 +19,23 @@ import java.text.DecimalFormat;
 public class RoverRuckusTeleOp extends OpMode{
     RoverRuckusHardware robot =  new RoverRuckusHardware();
     double left, right;
-
     double speedFactor;
-    double liftSpeed = -.5;
-
-   boolean brakeLast;
-   boolean brake;
 
     @Override
     public void init() {
         robot.init(hardwareMap);
-        brakeLast = false;
-        brake = false;
     }
 
     @Override
     public  void start() {
-       telemetry.addData("Lift Brake?", brake);
     }
 
     @Override
     public void loop() {
         driveTrainSpeed();
-        liftBrake();
 
         left = -Math.pow(gamepad1.left_stick_y, 3) * speedFactor;
         right = -Math.pow(gamepad1.right_stick_y, 3) * speedFactor;
-
-        if (brake == true)
-            liftSpeed = -0.5;
-        else
-            liftSpeed =  -Math.pow(gamepad2.left_stick_y, 3);
 
         powSet();
         telemetry();
@@ -60,13 +46,10 @@ public class RoverRuckusTeleOp extends OpMode{
 
         //telemetry.addData("Gamepad 1", gamepad1);
         //telemetry.addData("Gamepad 2", gamepad2);
-        telemetry.addData("Lift Pos",robot.lift.getCurrentPosition());
-        telemetry.addData("Lift2 Pos",robot.lift2.getCurrentPosition());
+
         telemetry.addData("Speed Factor", printFormat.format(speedFactor));
         telemetry.addData("Left", printFormat.format(left));
         telemetry.addData("Right", printFormat.format(right));
-        telemetry.addData("Lift Power", printFormat.format(liftSpeed));
-        telemetry.addData("Lift Brake?", brake);
     }
 
     void powSet() {
@@ -74,32 +57,14 @@ public class RoverRuckusTeleOp extends OpMode{
         robot.bl.setPower(left);
         robot.fr.setPower(right);
         robot.br.setPower(right);
-        robot.lift.setPower(liftSpeed);
-        robot.lift2.setPower(liftSpeed);
     }
 
     void driveTrainSpeed(){
         if(gamepad1.left_bumper && !gamepad1.right_bumper) //Drivetrain Speed Controls
-         speedFactor = 1;
+         speedFactor = 0.5;
        else if(gamepad1.right_bumper && !gamepad1.left_bumper)
-          speedFactor = 0.1;
+          speedFactor = 0.05;
         else if(!gamepad1.right_bumper && !gamepad1.left_bumper)
-          speedFactor = 0.55;
+          speedFactor = 0.275;
     }
-
-    void liftBrake() {
-            boolean brakePressed = gamepad2.a;
-
-            if(brakePressed && !brakeLast){
-               brake = !brake;
-               if(brake) {
-                  //while(robot.lift.getCurrentPosition() <= 540 && robot.lift2.getCurrentPosition() <= 540 && brake) {
-                   liftSpeed = -0.5;
-            //      }
-               }
-               else liftSpeed = Math.pow(gamepad2.left_stick_y, 3);
-            }
-            brakeLast = brakePressed;
-    }
-
 }

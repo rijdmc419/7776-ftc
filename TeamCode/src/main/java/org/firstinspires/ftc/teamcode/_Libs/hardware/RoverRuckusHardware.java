fd7efe9f21695182514f76a6a1d579851d5c231f
Scott3.0 <bremmerscottw@gmail.com>
2018-12-23 15:10:19 -0800

    extend-o-code (broken?)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Libs/hardware/RoverRuckusHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Libs/hardware/RoverRuckusHardware.java
@@ -13,11 +13,16 @@ public class RoverRuckusHardware {
     public DcMotor fl = null;
     public DcMotor bl = null;
     public DcMotor fr  = null;
-    public DcMotor br   = null;
+    public DcMotor br = null;
     public DcMotor joint = null;
     public DcMotor joint2 = null;
+    public DcMotor extend = null;
+    public DcMotor extend2 = null;
 
     public Servo markerServo;
+    public Servo flipServo;
+    public Servo intakeServo;
+    public Servo intakeServo2;
 
     HardwareMap hwMap = null;
 
@@ -32,15 +37,23 @@ public class RoverRuckusHardware {
         joint = hwMap.get(DcMotor.class, "joint");
         joint2 = hwMap.get(DcMotor.class, "joint2");
 
+        extend = hwMap.get(DcMotor.class, "extend");
+        extend2 = hwMap.get(DcMotor.class, "extend2");
+
+        flipServo = hwMap.get(Servo.class, "flipServo");
+        intakeServo = hwMap.get(Servo.class, "intakeServo");
+        intakeServo2 = hwMap.get(Servo.class, "intakeServo2");
         markerServo = hwMap.get(Servo.class, "markerServo");
 
         fl.setDirection(DcMotor.Direction.REVERSE);
         bl.setDirection(DcMotor.Direction.REVERSE);
         joint2.setDirection(DcMotor.Direction.REVERSE);
+        extend2.setDirection(DcMotor.Direction.REVERSE);
 
         fr.setDirection(DcMotor.Direction.FORWARD);
         br.setDirection(DcMotor.Direction.FORWARD);
         joint.setDirection(DcMotor.Direction.FORWARD);
+        extend.setDirection(DcMotor.Direction.FORWARD);
 
         fl.setPower(0);
         bl.setPower(0);
@@ -48,6 +61,8 @@ public class RoverRuckusHardware {
         br.setPower(0);
         joint.setPower(0);
         joint2.setPower(0);
+        extend.setPower(0);
+        extend2.setPower(0);
 
         fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -55,6 +70,8 @@ public class RoverRuckusHardware {
         br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         joint.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         joint2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        extend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        extend2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -62,7 +79,10 @@ public class RoverRuckusHardware {
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         joint.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         joint2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        extend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        extend2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         markerServo.setPosition(-1);
+        flipServo.setPosition(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/RoverRuckusTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/RoverRuckusTeleOp.java
@@ -21,7 +21,9 @@ public class RoverRuckusTeleOp extends OpMode{
     double left, right;
     double speedFactor;
     double jointSpeed;
-    double jointSpeedFactor = 1;
+    double jointSpeedFactor = 0.5;
+    double extendSpeed;
+    double extendSpeedFactor = 0.1;
 
     @Override
     public void init() {
@@ -39,13 +41,23 @@ public class RoverRuckusTeleOp extends OpMode{
         left = -Math.pow(gamepad1.left_stick_y, 3) * speedFactor;
         right = -Math.pow(gamepad1.right_stick_y, 3) * speedFactor;
 
-        joint();
+        jointSpeed = -Math.pow(gamepad2.left_stick_y, 3)* jointSpeedFactor;
+
+        extend();
         powSet();
         telemetry();
     }
 
-    void joint(){
-        jointSpeed = -Math.pow(gamepad2.left_stick_y, 3)* jointSpeedFactor;
+    void extend(){
+        if(gamepad2.left_trigger > 0){
+            extendSpeed = gamepad2.left_trigger * extendSpeedFactor * -1;
+        }
+        else if(gamepad2.right_trigger > 0){
+            extendSpeed = gamepad2.right_trigger * extendSpeedFactor;
+        }
+        else{
+            extendSpeed = 0;
+        }
     }
 
     void telemetry(){
@@ -54,6 +66,7 @@ public class RoverRuckusTeleOp extends OpMode{
         //telemetry.addData("Gamepad 1", gamepad1);
         //telemetry.addData("Gamepad 2", gamepad2);
 
+        telemetry.addData("Extend", printFormat.format(extendSpeed));
         telemetry.addData("Joint", printFormat.format(jointSpeed));
         telemetry.addData("Speed Factor", printFormat.format(speedFactor));
         telemetry.addData("Left", printFormat.format(left));
@@ -67,14 +80,16 @@ public class RoverRuckusTeleOp extends OpMode{
         robot.br.setPower(right);
         robot.joint.setPower(jointSpeed);
         robot.joint2.setPower(jointSpeed);
+        robot.extend.setPower(extendSpeed);
+        robot.extend2.setPower(extendSpeed);
     }
 
     void driveTrainSpeed(){
         if(gamepad1.left_bumper && !gamepad1.right_bumper) //Drivetrain Speed Controls
-         speedFactor = 0.5;
+         speedFactor = 1;
        else if(gamepad1.right_bumper && !gamepad1.left_bumper)
-          speedFactor = 0.05;
+          speedFactor = 0.5;
         else if(!gamepad1.right_bumper && !gamepad1.left_bumper)
-          speedFactor = 0.275;
+          speedFactor = 0.75;
     }
 }
